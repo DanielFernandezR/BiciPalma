3ce096dbd86930d6fc8f76c456134549f5b3421a
DanielFernandezR <dfernandez@cifpfbmoll.eu>
2020-02-27 15:00:27 +0100

    style(): Eliminar espacios en blanco.

--- src/main/java/edu/elsmancs/estacion/Estacion.java
+++ src/main/java/edu/elsmancs/estacion/Estacion.java
@@ -10,37 +10,36 @@ public class Estacion {
 	private int id = 0;
 	private String direccion = null;
 	private int numeroAnclajes = 0;
-	private Bicicleta[] anclajes = null; 
-	
+	private Bicicleta[] anclajes = null;
+
 	public Estacion(int id, String direccion, int anclajes) {
 		this.id = id;
 		this.direccion = direccion;
-		this.numeroAnclajes= anclajes;
+		this.numeroAnclajes = anclajes;
 		this.anclajes = new Bicicleta[anclajes];
 	}
-	
+
 	public int getID() {
 		return this.id;
 	}
-	
+
 	public String getDireccion() {
 		return this.direccion;
 	}
-	
+
 	public int getNumAnclajes() {
 		return this.numeroAnclajes;
 	}
 
-	
 	public void consultarEstacion() {
 		System.out.println("id: " + getID());
 		System.out.println("direccion: " + getDireccion());
 		System.out.println("numeroAnclajes: " + getNumAnclajes());
 	}
-	
+
 	public int anclajesLibres() {
 		int numAnclajesLibres = 0;
-		for (Bicicleta anclaje:this.anclajes){
+		for (Bicicleta anclaje : this.anclajes) {
 			if (anclaje == null) {
 				numAnclajesLibres += 1;
 			} else
@@ -48,74 +47,73 @@ public class Estacion {
 		}
 		return numAnclajesLibres;
 	}
-	
+
 	public void anclarBicicleta(Bicicleta bicicleta) {
 		int posicionLista = 0;
 		int numAnclaje = posicionLista + 1;
 		boolean biciAnclada = false;
-		
+
 		while (biciAnclada == false) {
-			
-			if(this.anclajes[posicionLista] == null) {
+
+			if (this.anclajes[posicionLista] == null) {
 				this.anclajes[posicionLista] = bicicleta;
 				mostrarAnclaje(bicicleta, numAnclaje);
 				biciAnclada = true;
-			}else { 
-					posicionLista ++;
-					numAnclaje ++;	
-				}
+			} else {
+				posicionLista++;
+				numAnclaje++;
+			}
 		}
 	}
-	
+
 	public void consultarAnclajes() {
 		int posicion = 0;
 		int numAnclaje = 0;
-		for (Bicicleta anclaje:this.anclajes) {
+		for (Bicicleta anclaje : this.anclajes) {
 			numAnclaje = posicion + 1;
 			if (anclaje == null) {
 				System.out.println("Anclaje " + numAnclaje + " libre");
-				posicion ++;
-			}
-			else {
+				posicion++;
+			} else {
 				System.out.println("Anclaje " + numAnclaje + " " + this.anclajes[posicion].getId());
-				posicion ++;
+				posicion++;
 			}
 		}
 	}
-	
+
 	public boolean leerTarjetaUsuario(TarjetaUsuario tarjetaUsuario) {
-		return tarjetaUsuario.getTarjetaActivada();
-		}
-	
+		return tarjetaUsuario.isTarjetaActivada();
+	}
+
 	public void retirarBicicleta(TarjetaUsuario tarjetaUsuario) {
-		if(leerTarjetaUsuario(tarjetaUsuario)) {
-			
+		if (leerTarjetaUsuario(tarjetaUsuario)) {
+
 			boolean biciRetirada = false;
-			
-			while(biciRetirada != true) {
+
+			while (biciRetirada != true) {
 				int posicion = generarAnclaje();
 				int numAnclaje = posicion + 1;
-				if(this.anclajes[posicion] != null) {
+				if (this.anclajes[posicion] != null) {
 					mostrarBicicleta(this.anclajes[posicion], numAnclaje);
 					this.anclajes[posicion] = null;
 					biciRetirada = true;
-					}else;
+				} else
+					;
 			}
 		}
 	}
-	
+
 	void mostrarAnclaje(Bicicleta bicicleta, int numAnclaje) {
 		System.out.println("bicicleta: " + bicicleta.getId() + " anclada en el anclaje: " + numAnclaje);
 	}
-	
+
 	void mostrarBicicleta(Bicicleta bicicleta, int numAnclaje) {
 		System.out.println("Bicicleta retirada: " + bicicleta.getId() + " del anclaje: " + numAnclaje);
 	}
-	
+
 	public int generarAnclaje() {
 		Integer numRandomAnclaje = ThreadLocalRandom.current().nextInt(0, this.anclajes.length);
 		return numRandomAnclaje;
 	}
-	
-}
 
+}
